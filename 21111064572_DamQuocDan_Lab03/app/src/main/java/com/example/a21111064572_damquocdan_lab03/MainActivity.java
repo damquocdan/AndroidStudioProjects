09ce172910d4a31d021cec1c9a809bbfab35eb1d
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-01-20 22:01:50 +0700

    20/01/2024

--- 21111064572_DamQuocDan_Lab03/app/src/main/java/com/example/a21111064572_damquocdan_lab03/MainActivity.java
+++ 21111064572_DamQuocDan_Lab03/app/src/main/java/com/example/a21111064572_damquocdan_lab03/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lesson03;
+package com.example.a21111064572_damquocdan_lab03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bai3);
     }
 }
